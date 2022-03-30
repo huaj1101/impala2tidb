ddf3735c986d9c107a0a5272ed8b3c98f7b9044c
huaj <huaj@mctech.vip>
2022-03-30 12:47:50 +0800

    自行生成csv

--- get_impala_db_data.py
+++ get_impala_db_data.py
@@ -16,56 +16,48 @@ sql_times = {}
 lock = threading.Lock()
 finish_count = 0
 dbs_created = set()
+BATCH_SIZE = 1e6
+
+def save_to_csv(df, file_name):
+    df.to_csv(f'data/{file_name}',sep='\u0001',na_rep='\\N',line_terminator='\u0002', header=False,index=False)
 
 @utils.thread_method
-def copy_to_text_table(db, table_schema, total_count):
+def export_to_csv(table_schema, total_count):
     start = time.time()
     if not hasattr(thread_context, 'cursor'):
         thread_context.cursor = utils.get_impala_cursor()
-    text_db = db + '_text'
-    lock.acquire()
-    if text_db not in dbs_created:
-        thread_context.cursor.execute(f'drop database if exists {text_db} cascade')
-        thread_context.cursor.execute(f'create database {text_db}')
-        time.sleep(2)
-        dbs_created.add(text_db)
-    lock.release()
+    table_full_name = table_schema['table']
+    db = table_full_name.split('.')[0].strip('`')
+    table = table_full_name.split('.')[1].strip('`')
 
-    ori_table = table_schema["table"]
-    new_table = text_db + '.' + ori_table.split('.')[1]
-    sql = f'/*& global:true*/ create table {new_table} stored as textfile as select * from {ori_table}'
-    thread_context.cursor.execute(sql)
-    thread_context.cursor.execute(f'refresh {new_table}')
-
+    batch_count = 1
+    has_id_field = False
+    for cs in table_schema['columns']:
+        if cs['name'] == '`id`' and cs['type'] == 'bigint':
+            has_id_field = True
+    if has_id_field:
+        sql = f'/*& global:true*/ select count(*) as cnt from {table_full_name}'
+        thread_context.cursor.execute(sql)
+        df = as_pandas(thread_context.cursor)
+        record_count = df.at[0, 'cnt']
+        batch_count = int(record_count / BATCH_SIZE) + 1
+    if batch_count == 1:
+        sql = f'/*& global:true*/ select * from {table_full_name}'
+        thread_context.cursor.execute(sql)
+        df = as_pandas(thread_context.cursor)
+        save_to_csv(df, f'{db}.{table}.001.csv')
+    else:
+        for i in range(batch_count):
+            sql = f'/*& global:true*/ select * from {table_full_name} where id % {batch_count} = {i}'
+            thread_context.cursor.execute(sql)
+            df = as_pandas(thread_context.cursor)
+            save_to_csv(df, f'{db}.{table}.{i+1:0>3d}.csv')
     time_used = time.time() - start
 
     global finish_count
     lock.acquire()
     finish_count += 1
-    logger.info('(%d / %d) %s finish in %.1f seconds' % (finish_count, total_count, ori_table, time_used))
-    lock.release()
-
-@utils.thread_method
-def download_table_csv(db, table, total_count):
-    start = time.time()
-    if not hasattr(thread_context, 'hdfsclient'):
-        thread_context.hdfsclient = utils.get_hdfs_client()
-    hdfs_path = f'/user/hive/warehouse/{db}.db/{table}'
-    csv_files = []
-    for item in thread_context.hdfsclient.list(hdfs_path):
-        if item.endswith('.'):
-            csv_files.append(item)
-    csv_files.sort()
-    for i, file in enumerate(csv_files):
-        hdfs_file = f'/user/hive/warehouse/{db}.db/{table}/{file}'
-        local_file = f'data/{db.replace("_text", "")}.{table}.{i+1:0>3d}.csv'
-        thread_context.hdfsclient.download(hdfs_file, local_file)
-
-    time_used = time.time() - start
-    global finish_count
-    lock.acquire()
-    finish_count += 1
-    logger.info('(%d / %d) %s.%s finish in %.1f seconds' % (finish_count, total_count, db, table, time_used))
+    logger.info('(%d / %d) %s finish in %.1f seconds' % (finish_count, total_count, table, time_used))
     lock.release()
 
 def main():
@@ -80,23 +72,14 @@ def main():
     pool = ThreadPoolExecutor(max_workers=threads)
     table_schemas = []
     for file in files:
-        db = file.replace('.json', '')
         with open(f'schemas/{file}', 'r', encoding='utf-8') as f:
             schema_text = f.read()
             db_schema = json.loads(schema_text)
             table_schemas.extend(db_schema)
 
-    # logger.info('copy_to_text_table')
-    # for table_schema in table_schemas:
-    #     db = table_schema['table'].split('.')[0].strip('`')
-    #     pool.submit(copy_to_text_table, db, table_schema, len(table_schemas))
-    # pool.shutdown(wait=True)
-
-    logger.info('download_table_csv')
+    logger.info('export_to_csv')
     for table_schema in table_schemas:
-        db = table_schema['table'].split('.')[0].strip('`') + '_text'
-        table = table_schema['table'].split('.')[1].strip('`')
-        pool.submit(download_table_csv, db, table, len(table_schemas))
+        pool.submit(export_to_csv, table_schema, len(table_schemas))
     pool.shutdown(wait=True)
 
     logger.info('finish in %.1f seconds' % (time.time() - start))
