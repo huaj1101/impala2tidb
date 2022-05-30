c211d992919702076f14a44a8dd712ba98813944
huaj <huaj@mctech.vip>
2022-05-30 16:52:25 +0800

    特殊处理excel_task_id字段

--- get_impala_db_schema.py
+++ get_impala_db_schema.py
@@ -66,7 +66,10 @@ def get_table_schema_kudu(db, table, cursor, df_columns):
             value = df_columns.at[0, name]
             name = f'`{name}`'
             for cs in columns:
-                if cs['name'] == name:
+                # 这个字段经常长度不够用，做大一点
+                if cs['name'] == '`excel_task_id`':
+                    cs['len'] = 1000
+                elif cs['name'] == name:
                     cs['len'] = int(value)
     return {
         'table': f'`{db}`.`{table}`', 
